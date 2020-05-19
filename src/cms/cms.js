5f0c7e47936490fb2b300a6733f7ca9b980ae893
Jack Behar <jbehar@taylordigital.io>
2020-05-19 16:52:50 -0400

    editor preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,4 +2,11 @@ import CMS from 'netlify-cms-app'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
+import { ctaEditorConfig } from "./editor-components/call-to-action.editor"
+
+
+
 CMS.registerPreviewTemplate('button', IndexPagePreview)
+
+// Extend editor
+CMS.registerEditorComponent(ctaEditorConfig)
